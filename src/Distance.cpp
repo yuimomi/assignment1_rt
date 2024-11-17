84645b9330e8797040e2605f6975b254e5f8963d
yuimomi <yuimomi0513@gmail.com>
2024-11-17 11:51:06 +0000

    Add the function to check the relative distance between turtle1 and turtle2 in src/Distance.cpp

---
+++ src/Distance.cpp
@@ -0,0 +1,63 @@
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include "std_msgs/F;oat32.h"
+#include <cmatch>
+
+ros::Publisher dist_pub;
+ros::Publisher pub_turtle1, pub_turtle2;
+
+double x1,x2,y1,y2;
+
+void poseCallback1(const turtlesim::Pose::ConstPtr &msg){
+    x1 = msg ->x;
+    y1 = msg ->y;
+}
+
+void poseCallback2(const turtlesim::Pose::ConstPtr &msg){
+    x2 = msg ->x;
+    y2 = msg ->y;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "distance_code");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub1 = nh.subscribe("turtle1/pose", 10, poseCallback1);
+    ros::Subscriber sub2 = nh.subscribe("turtle2/pose", 10, poseCallback2);
+
+    dist_pub = nh.advertise<std_msgs::Float32>("turtle_distance",10);
+    pub_turtle1 = nh.advertise<std_msgs::Twist>("turtle1/cmd_vel",10);
+    pub_turtle2 = nh.advertise<std_msgs::Twist>("turtle2/cmd_vel",10);
+
+    ros::Rate rate(10);
+    while(ros::ok()){
+        ros::spinOnce();
+        double distance = std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
+
+        // Publish distance
+        std_msgs::Float32 dist_msg;
+        dist_msg.data = distance;
+        dist_pub.publish(dist_msg);
+
+        // If the distance exceeds the threshold, stop the turtle
+        if (distance < 1.0){
+            geometry_msgs::Twist stop_cmd;
+            stop_cmd.linear.x = 0;
+            stop_cmd.angular.z = 0;
+            pub_turtle1.publish(stop_cmd);
+            pub_turtle2.publish(stop_cmd);
+        }
+        // If the turtle crosses the boundary, stop it
+        if (x1 < 1.0 || x1 > 10.0 || y1 < 1.0 || y1 > 10.0 || 
+            x2 < 1.0 || x2 > 10.0 || y2 < 1.0 || y2 > 10.0) {
+            geometry_msgs::Twist stop_cmd;
+            stop_cmd.linear.x = 0;
+            stop_cmd.angular.z = 0;
+            pub_turtle1.publish(stop_cmd);
+            pub_turtle2.publish(stop_cmd);
+        }
+    rate.sleep();
+    }
+return 0;
+}
+
